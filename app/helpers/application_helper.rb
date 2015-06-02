415eb7aed1281c868c05d3534e3619a0955a58c5
garthomp <garrettdthompson@gmail.com>
2015-06-01 19:59:09 -0500

    added carts and relationships

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  Date::DATE_FORMATS[:default] = "%e/%B/%Y"
+  Time::DATE_FORMATS[:default] = "%m/%d/%Y %I:%M %p"
+
+  def print_date
+    @datetime = DateTime.current.to_s
+  end
 end
